8bb62e89c55e3f722d5452b5833caa7b7d472e91
ozzux <osama.radi7@gmail.com>
2024-05-30 13:52:45 +0300

    Update training_yolo.py

--- training_yolo.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('best.pt')
-
-results = model.predict(source="https://www.shutterstock.com/image-photo/aerial-view-cargo-ship-business-260nw-1677971977.jpg",show=True, save=True)
