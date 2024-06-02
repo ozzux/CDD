c116667fd7d4adc424e32379362b40de57909242
ozzux <osama.radi7@gmail.com>
2024-06-02 22:36:07 +0300

    Update lesiondetection.py

--- lesiondetection.py
+++ lesiondetection.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO('yolomodel.pt')
 
-model.predict(source="C:/Users/osama/Pictures/Research/ADEM/ADEM_11.jpg",show=True)
+model.predict(source="IMG LOCATION",show=True) # substitute the image location
 
 input()
