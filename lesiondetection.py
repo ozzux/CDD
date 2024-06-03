70b502cfb2e18f566534d71586a86756ba9d4f71
ozzux <osama.radi7@gmail.com>
2024-06-03 16:05:51 +0300

    Update lesiondetection.py

--- lesiondetection.py
+++ lesiondetection.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('best.pt')
+model = YOLO('yolomodel.pt')
 
-model.predict(source="IMG SOURCE",show=True, conf=0.4, save=True) # substitute the image location
+model.predict(source="All Data\MS\MS_14.png",show=True, conf=0.4, save=True) # substitute the image location
 
 input("Press enter to exit.")
