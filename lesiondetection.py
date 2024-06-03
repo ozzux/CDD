ee1978fe888435fe0f49c4cd34ed8fb4dd8870a7
ozzux <osama.radi7@gmail.com>
2024-06-03 16:03:41 +0300

    retrained model on more data

--- lesiondetection.py
+++ lesiondetection.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('yolomodel.pt')
+model = YOLO('best.pt')
 
-model.predict(source="IMG SOURCE",show=True) # substitute the image location
+model.predict(source="IMG SOURCE",show=True, conf=0.4, save=True) # substitute the image location
 
 input("Press enter to exit.")
