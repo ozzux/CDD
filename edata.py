17514043190c0db9b09474bd5826d544066ac19f
ozzux <osama.radi7@gmail.com>
2024-06-17 09:21:28 -0700

    wow

--- edata.py
+++ edata.py
@@ -1,22 +1,21 @@
 import nibabel as nib
 import matplotlib.pyplot as plt
 import os
-
-'''img = nib.load("segtraindata/Patient-42/42-LesionSeg-Flair.nii").get_fdata()
-img = img[:, :, img.shape[2]//2]
-plt.imshow(img, cmap='gray')
-plt.show()'''
-
-def nii_to_img(nii_path):
-    img = nib.load(nii_path).get_fdata()
-    img = img[:, :, img.shape[2]//2]
-    return img
+import numpy as np
+import cv2
 
 
-print(os.listdir('segtraindata'))
+for patient in os.listdir('train'):
+    num = int(patient[8:])
+    print(str(num))
+    img1 = nib.load('train/Patient-'+str(num)+'/'+str(num)+'-LesionSeg-Flair.nii').get_fdata()
+    img1 = img1[:, :, img1.shape[2]//2]
 
-for folder in os.listdir('segtraindata'):
-    path = 'segtraindata/'+folder
-    i = int(folder[8:])
-    img = nii_to_img(path+'/'+str(i)+'-LesionSeg-Flair.nii')
-    plt.imsave(os.path.join('segtraindata3',str(i)+'.png'), img,cmap='gray')
+    img2 = nib.load('train/Patient-'+str(num)+'/'+str(num)+'-Flair.nii').get_fdata()
+    img2 = img2[:, :, img2.shape[2]//2]
+
+    plt.imsave('traindata/images/'+str(num)+'.jpg', img2, cmap='gray')
+    plt.imsave('traindata/masks/'+str(num)+'.jpg', img1, cmap='gray')
+
+
+
