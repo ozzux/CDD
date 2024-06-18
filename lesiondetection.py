fbc7fc5d31fc938fdd6d39ca2691fcbcac6cd991
ozzux <osama.radi7@gmail.com>
2024-06-17 18:09:36 -0700

    updated and cleaned data

--- lesiondetection.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolomodel.pt')
-
-model.predict(source="All Data\MS\MS_17.png", conf=0.4, save=True, show_labels=False) # substitute the image location
--- mtop.py
+++ mtop.py
@@ -1,40 +1,118 @@
+import json
 import os
+import argparse
+parser = argparse.ArgumentParser(description='Test yolo data.')
+parser.add_argument('-j', help='JSON file', dest='json', required=True)
+parser.add_argument('-o', help='path to output folder', dest='out',required=True)
 
-import cv2
+args = parser.parse_args()
 
+json_file = args.json 
+output = args.out 
+class COCO2YOLO:
+    def __init__(self):
+        self._check_file_and_dir(json_file, output)
+        self.labels = json.load(open(json_file, 'r', encoding='utf-8'))
+        self.coco_id_name_map = self._categories()
+        self.coco_name_list = list(self.coco_id_name_map.values())
+        print("total images", len(self.labels['images']))
+        print("total categories", len(self.labels['categories']))
+        print("total labels", len(self.labels['annotations']))
 
-input_dir = 'segtraindata/masks'
-output_dir = 'segtraindata/labels'
+    def _check_file_and_dir(self, file_path, dir_path):
+        if not os.path.exists(file_path):
+            raise ValueError("file not found")
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
 
-for j in os.listdir(input_dir):
-    image_path = os.path.join(input_dir, j)
-    # load the binary mask and get its contours
-    mask = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    _, mask = cv2.threshold(mask, 1, 255, cv2.THRESH_BINARY)
+    def _categories(self):
+        categories = {}
+        for cls in self.labels['categories']:
+            categories[cls['id']] = cls['name']
+        return categories
 
-    H, W = mask.shape
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def _load_images_info(self):
+        images_info = {}
+        for image in self.labels['images']:
+            id = image['id']
+            file_name = image['file_name']
+            if file_name.find('\\') > -1:
+                file_name = file_name[file_name.index('\\')+1:]
+            w = image['width']
+            h = image['height']
+            images_info[id] = (file_name, w, h)
 
-    # convert the contours to polygons
-    polygons = []
-    for cnt in contours:
-        if cv2.contourArea(cnt) > 200:
-            polygon = []
-            for point in cnt:
-                x, y = point[0]
-                polygon.append(x / W)
-                polygon.append(y / H)
-            polygons.append(polygon)
+        return images_info
 
-    # print the polygons
-    with open('{}.txt'.format(os.path.join(output_dir, j)[:-4]), 'w') as f:
-        for polygon in polygons:
-            for p_, p in enumerate(polygon):
-                if p_ == len(polygon) - 1:
-                    f.write('{}\n'.format(p))
-                elif p_ == 0:
-                    f.write('0 {} '.format(p))
-                else:
-                    f.write('{} '.format(p))
+    def _bbox_2_yolo(self, bbox, img_w, img_h):
+        x, y, w, h = bbox[0], bbox[1], bbox[2], bbox[3]
+        centerx = bbox[0] + w / 2
+        centery = bbox[1] + h / 2
+        dw = 1 / img_w
+        dh = 1 / img_h
+        centerx *= dw
+        w *= dw
+        centery *= dh
+        h *= dh
+        return centerx, centery, w, h
 
+    def _convert_anno(self, images_info):
+        anno_dict = dict()
+        for anno in self.labels['annotations']:
+            bbox = anno['bbox']
+            image_id = anno['image_id']
+            category_id = anno['category_id']
+
+            image_info = images_info.get(image_id)
+            image_name = image_info[0]
+            img_w = image_info[1]
+            img_h = image_info[2]
+            yolo_box = self._bbox_2_yolo(bbox, img_w, img_h)
+
+            anno_info = (image_name, category_id, yolo_box)
+            anno_infos = anno_dict.get(image_id)
+            if not anno_infos:
+                anno_dict[image_id] = [anno_info]
+            else:
+                anno_infos.append(anno_info)
+                anno_dict[image_id] = anno_infos
+        return anno_dict
+
+    def save_classes(self):
+        sorted_classes = list(map(lambda x: x['name'], sorted(self.labels['categories'], key=lambda x: x['id'])))
+        print('coco names', sorted_classes)
+        with open('coco.names', 'w', encoding='utf-8') as f:
+            for cls in sorted_classes:
+                f.write(cls + '\n')
         f.close()
+
+    def coco2yolo(self):
+        print("loading image info...")
+        images_info = self._load_images_info()
+        print("loading done, total images", len(images_info))
+
+        print("start converting...")
+        anno_dict = self._convert_anno(images_info)
+        print("converting done, total labels", len(anno_dict))
+
+        print("saving txt file...")
+        self._save_txt(anno_dict)
+        print("saving done")
+
+    def _save_txt(self, anno_dict):
+        for k, v in anno_dict.items():
+            file_name = os.path.splitext(v[0][0])[0] + ".txt"
+            with open(os.path.join(output, file_name), 'w', encoding='utf-8') as f:
+                print(k, v)
+                for obj in v:
+                    cat_name = self.coco_id_name_map.get(obj[1])
+                    category_id = self.coco_name_list.index(cat_name)
+                    box = ['{:.6f}'.format(x) for x in obj[2]]
+                    box = ' '.join(box)
+                    line = str(category_id) + ' ' + box
+                    f.write(line + '\n')
+
+
+if __name__ == '__main__':
+    c2y = COCO2YOLO()
+    c2y.coco2yolo()
