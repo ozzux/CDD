f97dd4d8e91e6322e9f2d7cedc0949633b093e50
ozzux <osama.radi7@gmail.com>
2024-06-21 11:43:08 -0700

    updated model

--- pipeline.py
+++
@@ -1,9 +0,0 @@
-from seg_ind import seg
-import matplotlib.pyplot as plt
-import numpy as np
-import tensorflow as tf
-
-
-res = seg('convdata/MS/MS_5.png')/255
-
-model = tf.keras.models.load_model('model.keras')
