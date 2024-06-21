7f499ecdebda06f18ac6994ab7038140f12d5f03
ozzux <osama.radi7@gmail.com>
2024-06-21 10:57:42 -0700

    added model

--- pipeline.py
+++ pipeline.py
@@ -2,9 +2,8 @@ from seg_ind import seg
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-import keras
+
 
 res = seg('convdata/MS/MS_5.png')/255
 
-model = keras.models.load_model('model.h5')
-model.predict(res)
+model = tf.keras.models.load_model('model.keras')
